6dfcc0ad9ef59e44709c875f28e688b344409449
Tanner Paige <69323487+tanner-paige@users.noreply.github.com>
2023-03-15 19:23:32 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,93 +1,59 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const fs = require('fs')
+let messages = [];
 
-const app = express()
+function saveMessage() {
+  const messageText = document.getElementById("message-input").value.trim();
+  if (messageText === "") {
+    alert("Please enter a message before submitting");
+    return;
+  }
 
-app.use(express.static('public'))
-app.use(bodyParser.json())
+  const newMessage = { text: messageText, votes: 0 };
+  messages.push(newMessage);
 
-app.get('/messages', getMessages)
-app.post('/messages', postMessage)
-app.put('/messages/:id/vote', voteMessage)
-
-function getMessages(request, response) {
-  fs.readFile('./messages.json', 'utf8', function(err, data) {
-    if (err) {
-      console.error(err)
-      response.status(500).send('Server Error')
-    } else {
-      response.setHeader('Content-Type', 'application/json')
-      response.send(data)
-    }
+  fetch("./messages.json", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(messages),
   })
+    .then(() => {
+      loadMessages();
+      document.getElementById("message-input").value = "";
+    })
+    .catch((error) => {
+      console.error("Error saving messages:", error);
+    });
 }
 
-function postMessage(request, response) {
-  var newMessage = request.body
-
-  fs.readFile('./messages.json', 'utf8', function(err, data) {
-    if (err) {
-      console.error(err)
-      response.status(500).send('Server Error')
-    } else {
-      var messages = JSON.parse(data)
-      var lastId = messages.length > 0 ? messages[messages.length - 1].id : 0
-      var newId = lastId + 1
-      var newMessageWithId = {
-        id: newId,
-        text: newMessage.text,
-        votes: 0
+function loadMessages() {
+  fetch("./messages.json")
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
       }
-      messages.push(newMessageWithId)
-      var json = JSON.stringify(messages)
-
-      fs.writeFile('./messages.json', json, 'utf8', function(err) {
-        if (err) {
-          console.error(err)
-          response.status(500).send('Server Error')
-        } else {
-          response.status(201).send(newMessageWithId)
-        }
-      })
-    }
-  })
-}
-
-function voteMessage(request, response) {
-  var id = parseInt(request.params.id)
-  var voteType = request.body.type
-  var voteValue = voteType === 'up' ? 1 : -1
-
-  fs.readFile('./messages.json', 'utf8', function(err, data) {
-    if (err) {
-      console.error(err)
-      response.status(500).send('Server Error')
-    } else {
-      var messages = JSON.parse(data)
-      var message = messages.find(function(m) {
-        return m.id === id
-      })
-
-      if (message) {
-        message.votes += voteValue
-        var json = JSON.stringify(messages)
-
-        fs.writeFile('./messages.json', json, 'utf8', function(err) {
-          if (err) {
-            console.error(err)
-            response.status(500).send('Server Error')
-          } else {
-            response.status(200).send(message)
-          }
-        })
-      } else {
-        response.status(404).send('Message not found')
+      return response.json();
+    })
+    .then((data) => {
+      messages = data;
+      const messageList = document.getElementById("message-list");
+      messageList.innerHTML = "";
+      for (let i = 0; i < messages.length; i++) {
+        const message = messages[i];
+        const listItem = document.createElement("li");
+        listItem.innerText = message.text;
+        messageList.appendChild(listItem);
       }
-    }
-  })
+    })
+    .catch((error) => {
+      console.error("Error loading messages:", error);
+    });
 }
 
-app.listen(3000, function() {
-  console.log('Server listening on port 3000')
-})
+loadMessages();
+
+document.getElementById("message-form").addEventListener("submit", (event) => {
+  event.preventDefault();
+  saveMessage();
+});
+
